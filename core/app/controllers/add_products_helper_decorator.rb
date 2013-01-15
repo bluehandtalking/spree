a062e53e51a1388923d7df77979ec1048a3c5c36
Jerrold Thompson <jet@whidbey.com>
2013-01-15 14:51:22 -0800

    Removed add_products_decorator_helper.rb as it is too early for it in load process

--- core/app/controllers/add_products_helper_decorator.rb
+++
@@ -1,8 +0,0 @@
-Spree::UserPasswordsController, 
-Spree::UserRegistrationsController, 
-Spree::StaticContentController,
-Spree::UserSessionsController].each do |klass|
-  klass.class_eval do
-    helper 'spree/products'
-  end
-end
