3d1d9b9b5c3b161d02e770360a39a6057c75e7cc
Jerrold Thompson <jet@whidbey.com>
2013-03-05 10:20:51 -0800

    Remove promotion_tax from default_tax

--- core/app/models/spree/calculator/default_tax.rb
+++ core/app/models/spree/calculator/default_tax.rb
@@ -22,39 +22,26 @@ module Spree
         self.calculable
       end
 
-     def compute_order(order)
-	matched_line_items = order.line_items.select do |line_item|
-	     line_item.product.tax_category == rate.tax_category
-	end
-	
-       line_items_total = matched_line_items.sum(&:total) 
+      def compute_order(order)
+        matched_line_items = order.line_items.select do |line_item|
+          line_item.product.tax_category == rate.tax_category
+        end
 
-       unless order.tax_cloud_transaction.nil?
-
-	    cloud_rate = order.tax_cloud_transaction.amount / ( line_items_total + order.ship_total )  
-
-	    unless order.adjustments.promotion.blank? 
-
-		adjusted_total = line_items_total + order.promotions_total + order.ship_total
-
-		unless adjusted_total.nil?  
-
-		   round_to_two_places( adjusted_total * cloud_rate ) 
-
-		else
-
-		   round_to_two_places(line_items_total * cloud_rate)
-
-		end
-	    end
-	
+        line_items_total = matched_line_items.sum(&:total)
+	 
+       unless order.adjustments.promotion.blank? 
+	  adjusted_total = line_items_total + order.promotions_total + order.ship_total
+	  unless adjusted_total.nil?  
+	     round_to_two_places( adjusted_total * rate.amount ) 
+	  else
+	     0
+	  end
        else
-
-	  round_to_two_places(line_items_total * rate.amount) 
-
+	     round_to_two_places(line_items_total * rate.amount) 
        end 
-     end
-      
+ 
+      end
+
       def compute_line_item(line_item)
         if line_item.product.tax_category == rate.tax_category
           deduced_total_by_rate(line_item.total, rate)
